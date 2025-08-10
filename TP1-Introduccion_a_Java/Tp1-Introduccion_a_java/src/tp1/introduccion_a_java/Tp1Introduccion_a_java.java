adb9ad93e045a06056afedea9ea6ed46f5ee1034
Ger <ger.kadar@gmail.com>
2025-08-10 15:03:16 -0300

    Carpeta con TP 1

---
+++ TP1-Introduccion_a_Java/Tp1-Introduccion_a_java/src/tp1/introduccion_a_java/Tp1Introduccion_a_java.java
@@ -0,0 +1,19 @@
+
+package tp1.introduccion_a_java;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Tp1Introduccion_a_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

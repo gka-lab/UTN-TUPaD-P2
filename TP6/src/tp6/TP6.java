865a661864775525b5f537f07f39bbc52479a58f
Ger <ger.kadar@gmail.com>
2025-10-20 17:53:01 -0300

    TP6

---
+++ TP6/src/tp6/TP6.java
@@ -0,0 +1,19 @@
+
+package tp6;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class TP6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
